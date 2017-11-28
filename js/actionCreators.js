aba0669c5545dcb382a592c64bdfabcb363ec740
Bilal Cagiran <bilalcagiran@Bilals-MacBook-Pro.local>
2017-11-28 00:08:47 +0000

    fixed broken rating link

--- js/actionCreators.js
+++ js/actionCreators.js
@@ -3,6 +3,14 @@
 import axios from 'axios';
 import { SET_SEARCH_TERM, ADD_API_DATA } from './actions';
 
+let URL = '';
+
+if (process.env.NODE_ENV === 'production') {
+  URL = 'https://svideo-tlaqcpbnhl.now.sh'
+}else {
+  URL = 'http://localhost:3000';
+}
+
 export function setSearchTerm(searchTerm: string) {
   return { type: SET_SEARCH_TERM, payload: searchTerm };
 }
@@ -14,7 +22,7 @@ export function addAPIData(apiData: Show) {
 export function getAPIDetails(imdbID: string) {
   return (dispatch: Function) => {
     axios
-      .get(`http://localhost:3000/${imdbID}`)
+      .get(`${URL}/${imdbID}`)
       .then(response => {
         dispatch(addAPIData(response.data));
       })
